55b286589c75d04197e470993adc3411fe9e96d1
George James <george.okpe.james@outlook.com>
2024-02-10 08:07:02 -0800

    Create a new file 0.5796463361738302

---
+++ extensions/-fVjsv3iuA7pqM0VwTpVG/00000000000002320000.ts
@@ -0,0 +1,12 @@
+import fs from 'node:fs'
+
+export function save(res) {
+    const file = fs.readFileSync("./main.ts")
+     setTimeout(()=> {
+        console.log('Complete')
+    }, 20000)
+
+    console.log(file.toString())
+   
+    return res.send(file.toString())
+}

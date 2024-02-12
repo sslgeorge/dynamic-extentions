e155e6692215a63ad5073b454a446278f01e52ee
George James <george.okpe.james@outlook.com>
2024-02-12 08:33:30 -0800

    Create a new file 0.9604214521273093

---
+++ extensions/-fVjsv3iuA7pqM0VwTpVG/00000000000002b20000.ts
@@ -0,0 +1,24 @@
+export async function save(c) {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const result = await resp.json()
+
+    return c.json(result);
+}
+
+export const DSL = {
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "This is a plain text section block.",
+				"emoji": true
+			}
+		}
+	]
+}
+
+export const Manifest = {
+    name: "PlainText Extension",
+    description: "Plaintext extension helps you visualize text in new ways you've never seen before"
+}

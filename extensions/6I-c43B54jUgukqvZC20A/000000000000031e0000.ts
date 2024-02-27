642d20d3c614b336bed59b1ca65b36311576bb53
George James <george.okpe.james@outlook.com>
2024-02-27 08:52:21 -0800

    Create a new file 0.19177882430326698

---
+++ extensions/6I-c43B54jUgukqvZC20A/000000000000031e0000.ts
@@ -0,0 +1,26 @@
+export async function save(data) {
+    return {
+        label: {
+            isValid: true
+        }
+
+    }
+}
+
+export const DSL = {
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "This is coming from another plaintext extension",
+				"emoji": true
+			}
+		}
+	]
+}
+
+export const Manifest = {
+    name: "Demo Extension",
+    description: "Plaintext extension helps you visualize text in new ways you've never seen before"
+}
